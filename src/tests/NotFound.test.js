c77a2e91ff34cc854840fdd6752c49b247529190
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-10-25 12:54:22 -0300

    Testes do componente NotFound completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Testando o NotFound', () => {
+  test('Testa se contém o h2 = Page requested not found', () => {
+    render(<NotFound />);
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(h2).toBeInTheDocument();
+  });
+  test('Testa se a página contém uma imagem', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

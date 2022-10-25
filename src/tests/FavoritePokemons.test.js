b8eca454a1ab4842150dbd255452bf4c8e534c5c
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-10-25 12:44:35 -0300

    Testes do componente FavoritePokemons completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+
+describe('Testa o componente Favorite Pokemons', () => {
+  test('Testa se é exibido na tela a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+    const pokemon = screen.getByText('No favorite pokemon found');
+    expect(pokemon).toBeInTheDocument();
+  });
+});

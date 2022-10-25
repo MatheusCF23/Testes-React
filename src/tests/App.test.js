366edd07b558bb45450d3f1f001318f49b760dfc
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-10-25 10:29:48 -0300

    Testes do componete App completo

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste App', () => {
+  test('Testando se o link possui o texto Home', () => {
+    renderWithRouter(<App />);
+    const Home = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(Home);
+    expect(Home).toBeInTheDocument();
+  });
+  test('Testando se o link possui o texto About', () => {
+    renderWithRouter(<App />);
+    const About = screen.getByRole('link', { name: /About/i });
+    userEvent.click(About);
+    expect(About).toBeInTheDocument();
+  });
+  test('Testando se o link possui o texto Favorite Pokemons', () => {
+    renderWithRouter(<App />);
+    const Favorite = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(Favorite);
+    expect(Favorite).toBeInTheDocument();
+  });
+});

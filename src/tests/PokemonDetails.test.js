1ccb0b247a3268d4ce3386c6f76fb7c89629e127
Matheus Chaves de Farias <matheusc.farias123@gmail.com>
2022-10-26 11:08:53 -0300

    Testes do componete PokemonDetails completo

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa Pokemon', () => {
+  test('Testa informações detalhadas', () => {
+    renderWithRouter(<App />);
+    const lkMore = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(lkMore);
+    const details = screen.getByRole('heading', { name: /details/i, level: 2 });
+    expect(details).toBeInTheDocument();
+    const nameSum = screen.getByRole('heading', { name: /summary/i, level: 2 });
+    expect(nameSum).toBeInTheDocument();
+    const location = screen.getByRole('heading', { name: /game locations/i, level: 2 });
+    expect(location).toBeInTheDocument();
+    const img = screen.getAllByRole('img', { name: /pikachu location/i });
+    expect(img[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(img[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+    const checkFavorite = screen.getByText(/Pokémon favoritado?/i);
+    expect(checkFavorite).toBeInTheDocument();
+    const summary = screen.getByText(/This intelligent Pokémon roasts/i);
+    expect(summary).toBeInTheDocument();
+  });
+});
